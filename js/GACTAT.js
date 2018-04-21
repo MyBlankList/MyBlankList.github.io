c8428c3f2e72fd46b96622dd2f15cc3babf62891
GiuseppeIII <33046979+GiuseppeIII@users.noreply.github.com>
2018-04-21 16:57:47 -0500

    GACAT

---
+++ js/GACTAT.js
@@ -0,0 +1,8 @@
+// GOOGLE ANALYTICS CTA TRACKING
+$('.btn').click(function() {
+var text = $(this).text();  // Grab the text from the button
+var index = $('.btn').index(this); // Grab the button index (what button it is on the page)
+var humanIndex = index + 1; // Button index usually starts at 0, so lets add 1 to make it human readable.
+var trackedText = text + ' (' + humanIndex + ')'; // Set the recorded value, for example "Download (3)" for the third download button on the page.
+ga('send', 'event', 'Button', 'Click', trackedText);
+});
